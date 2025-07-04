7923103753daae2057192d3ebe168589ff76bd10
TihanMo <104870528+TihanMo@users.noreply.github.com>
2025-07-04 13:14:44 +0200

    chore: enable screens in App

--- brain-nourishment-app/App.js
+++ brain-nourishment-app/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { enableScreens } from 'react-native-screens';
 import StackNavigator from './navigation/StackNavigator.jsx';
+
+// Improve memory usage and performance for navigation
+enableScreens();
 
 export default function App() {
   return (
